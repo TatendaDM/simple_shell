d7d93e89a7ade08ebc64ca76dcf13a5a543132ba
TatendaDM <127248571+TatendaDM@users.noreply.github.com>
2023-07-29 04:28:07 +0200

    Update _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,78 +1,64 @@
-#include "shell.h"
 /**
- * _atoi - .conversion process from string to integer
- * @s: String up for conversion
- * Return: 0 if invalid string, 1 valid number converted
- */
-
-int _atoi(char *s)
-
-{
-	int i, sign = 1, flag = 0, output;
-	unsigned int result = 0;
-
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
-	{
-		if (s[i] == '-')
-			sign *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		else if (flag == 1)
-			flag = 2;
-	}
-
-	if (sign == -1)
-		output = -result;
-	else
-		output = result;
-
-	return (output);
-}
-
-/**
- * interactive - True for an interactive mode
- * @info: struct address
+ * _atoi - converts a string to an integer
+ * @s: the string to convert
  *
- * Return: 0 non-interactive, 1 interactive mode
+ * Return: the converted integer
  */
-
-int interactive(info_t *info)
+int _atoi(char *s)
 {
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+int i, sign = 1, result = 0;
+
+if (s == NULL)
+return 0;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == '-')
+sign *= -1;
+
+if (s[i] >= '0' && s[i] <= '9')
+{
+result *= 10;
+result += (s[i] - '0');
+}
+else if (result != 0)
+{
+// Stop parsing if non-digit character is encountered after digits
+break;
+}
+}
+
+return result * sign;
 }
 
 /**
- * _isalpha - Alphabtc char checker
- * @c: Input Char
- * Return: 0 not aphabtc,1 alphabetic
+ * _isalpha - checks for alphabetic character
+ * @c: The character to check
+ *
+ * Return: 1 if c is alphabetic, 0 otherwise
  */
-
 int _isalpha(int c)
-
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return 1;
+else
+return 0;
 }
 
 /**
- * is_delim - delimiter char checker
- * @c: Check the delimiter char
- * @delim: the delimiter string
- * Return: 1 if true, 0 if false
+ * is_delim - checks if a character is a delimiter
+ * @c: The character to check
+ * @delim: The delimiter string
+ *
+ * Return: 1 if c is a delimiter, 0 otherwise
  */
-
 int is_delim(char c, char *delim)
-
 {
-	while (*delim)
-		if (*delim++ == c)
-			return (1);
-	return (0);
+while (*delim)
+{
+if (*delim++ == c)
+return 1;
+}
+
+return 0;
 }
